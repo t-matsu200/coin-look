4161bbc3487b90a72552357c6d482b3c9970b8c2
t.m <norakuro8386@gmail.com>
2021-11-29 19:42:25 +0900

    fix

--- src/pages/ieo.tsx
+++ src/pages/ieo.tsx
@@ -12,7 +12,7 @@ export default function IEOComponent() {
     return (
         <MainLayout>
             <Seo pageTitle="【Coincheck IEO】2022年夏にCoincheckとFINANCIE（フィナンシェ）がIEOによるトークン発行！"
-                pageDescription='CoincheckとFINANCIEがIEOによる資金調達実現に向けて契約を締結しました。この記事ではその詳細について説明していきます。' />
+                pageDescription='CoincheckとFINANCIEがIEOによる資金調達実現に向けた契約締結を発表しました。この記事ではその詳細について説明していきます。' />
             <BreadCrunb itemList={[{name: 'IEO', path: '/ieo'}]} />
             <div>
                 <div className={styles.display_flex}>
